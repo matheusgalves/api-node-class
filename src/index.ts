a7fb4fab9a7fdbc542f7526b58045f0f49fbd5cf
Matheus Alves <matheusgalves.0604@gmail.com>
2025-04-07 16:24:05 -0300

    feat: update project configuration and add initial routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { server } from "./server/Server";
 
-server.listen(3333,  () => {
-    console.log("Server is running on port 303300");
+
+server.listen(process.env.PORT || 3333,  () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
 });
